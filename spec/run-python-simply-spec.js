48c09645740e52f8e0461bd05e5833b720a43ee5
yitian <lovery521@gmail.com>
2018-12-10 19:00:27 +0800

    delete spec

--- spec/run-python-simply-spec.js
+++ spec/run-python-simply-spec.js
@@ -1,73 +1,31 @@
 'use babel';
 
-import Fuck from '../lib/fuck';
+import Runner from '../lib/run-python-simply';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('Fuck', () => {
+describe('Run python simply', () => {
   let workspaceElement, activationPromise;
-
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('fuck');
+    activationPromise = atom.packages.activatePackage('run-python-simply');
   });
 
-  describe('when the fuck:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.fuck')).not.toExist();
-
+  describe('when trigger the runner', () => {
+    it('print the log', () => {
       // This is an activation event, triggering it will cause the package to be
       // activated.
-      atom.commands.dispatch(workspaceElement, 'fuck:toggle');
+      atom.commands.dispatch(workspaceElement, 'rum-python-simply:toggle');
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
+      // waitsForPromise(() => {
+      //   return activationPromise;
+      // });
 
-      runs(() => {
-        expect(workspaceElement.querySelector('.fuck')).toExist();
 
-        let fuckElement = workspaceElement.querySelector('.fuck');
-        expect(fuckElement).toExist();
-
-        let fuckPanel = atom.workspace.panelForItem(fuckElement);
-        expect(fuckPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'fuck:toggle');
-        expect(fuckPanel.isVisible()).toBe(false);
-      });
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.fuck')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'fuck:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let fuckElement = workspaceElement.querySelector('.fuck');
-        expect(fuckElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'fuck:toggle');
-        expect(fuckElement).not.toBeVisible();
-      });
-    });
   });
 });
